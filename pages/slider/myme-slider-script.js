9231cf899dd419749ded784d60dcdc6f7e321a9f
sdeffff <41970421+sdeffff@users.noreply.github.com>
2023-07-08 05:56:27 +0300

    Add files via upload

---
+++ pages/slider/myme-slider-script.js
@@ -0,0 +1,61 @@
+const sliderBtn = document.querySelectorAll('.slider-btn'),
+sliderBtns = [...sliderBtn],
+
+sliderImgs = document.querySelector(".slider-imgs"),
+sliderImg = document.querySelector(".slider-img");
+
+sliderBtns[0].addEventListener("click", () => {
+    sliderImgs.style.marginLeft = '0'
+
+    sliderBtns[0].classList.add('active');
+    sliderBtns[1].classList.remove('active');
+    sliderBtns[2].classList.remove('active');
+})
+
+sliderBtns[1].addEventListener("click", () => {
+    sliderImgs.style.marginLeft = '-865px'
+
+    sliderBtns[0].classList.remove('active');
+    sliderBtns[1].classList.add('active');
+    sliderBtns[2].classList.remove('active');
+})
+
+sliderBtns[2].addEventListener("click", () => {
+    sliderImgs.style.marginLeft = '-1740px'
+
+    sliderBtns[0].classList.remove('active');
+    sliderBtns[1].classList.remove('active');
+    sliderBtns[2].classList.add('active');
+})
+
+if(window.innerWidth >= 1536) {
+    sliderBtns[0].addEventListener("click", () => {
+        sliderImgs.style.marginLeft = '0'
+    })
+    
+    sliderBtns[1].addEventListener("click", () => {
+        sliderImgs.style.marginLeft = '-865px'
+    })
+    
+    sliderBtns[2].addEventListener("click", () => {
+        sliderImgs.style.marginLeft = '-1740px'
+    })
+
+    console.log('asd')
+} else {
+    sliderBtns[0].addEventListener("click", () => {
+        sliderImgs.style.marginLeft = '0'
+    })
+    
+    sliderBtns[1].addEventListener("click", () => {
+        sliderImgs.style.marginLeft = '-680px'
+    })
+    
+    sliderBtns[2].addEventListener("click", () => {
+        sliderImgs.style.marginLeft = '-1360px'
+    })
+
+    console.log('fas')
+}
+
+console.log(window.screen);
